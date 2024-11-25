f8c648800d85d5f85ca9201e93fe97de3e671875
officialputuid <officialputuid@hack.id>
2024-11-25 10:03:12 +0800

    xNodepayBot: Updated active API ping domains
    Change-Id: I6378f64676459b1377621224a60a32162337c252

--- main.py
+++ main.py
@@ -48,10 +48,15 @@ DOMAIN_API_ENDPOINTS = {
         "https://api.nodepay.ai/api/auth/session"
     ],
     "PING": [
-        "http://3.1.154.253/api/network/ping",
         "http://13.215.134.222/api/network/ping",
+        "http://18.136.143.169/api/network/ping",
         "http://18.139.20.49/api/network/ping",
-        "http://52.74.35.173/api/network/ping"
+        "http://18.142.214.13/api/network/ping",
+        "http://18.142.29.174/api/network/ping",
+        "http://3.1.154.253/api/network/ping",
+        "http://52.74.35.173/api/network/ping",
+        "http://52.77.10.116/api/network/ping",
+        "http://nodepaypantek.dayon.me/api/network/ping"
     ]
 }
 
